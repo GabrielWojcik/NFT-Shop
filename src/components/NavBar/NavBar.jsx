ced9b7cd03916e71640072bfddfb8f81c50ad64a
GabrielWojcik <gabrielwojcikv@gmail.com>
2022-10-20 19:31:49 -0300

    FEAT - NavBar - menu mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 import Image from "next/image"
 import styles from "./Styles.module.css";
 import NftGo from "../../../public/NFTGo svg.png";
-import { IoMdMenu } from "react-icons/io";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 export default function NavBar() {
+    
+    const[openMenu, setOpenMenu] = useState(false)
+
     return(
         <div className={styles.container}>
             <p>Collections</p>
@@ -15,9 +19,27 @@ export default function NavBar() {
                 <Image src={NftGo} width={241} height={58} id={styles.logo} />
             </Link>
                 <div className={styles.hamburger}>
-                    <IoMdMenu size={45} color="#9DFE00" />
+                    <IoMdMenu size={45} color="#9DFE00" onClick={() => setOpenMenu(true)} />
                 </div>
-            
+            {
+                openMenu === true ?
+                <div className={styles.menuOpenContainer}>
+                    <div className={styles.closeMenu}>
+                        <IoMdClose size={45} color="#9DFE00" onClick={() => setOpenMenu(false)} />
+                        <p>home</p>
+                    </div>
+                    <div className={styles.menuOptions}>
+                        <h3>Home</h3>
+                        <h3>Whitelist</h3>
+                        <h3>Contacts</h3>
+                    </div>
+                </div>                
+                :
+                <>
+                </>
+            }
+
+
             <p>Tops</p>
             <p>Whitelist</p>
             <p>Contacts</p>
